b38c26a42d48633f81f74139f524bf9282b762d5
Mauricio Cox <coxmau77@gmail.com>
2024-03-29 17:54:12 -0300

    chore: correcciones de rutasa

--- clase_13_1/test-react-vite/src/components/App/App.jsx
+++ clase_13_1/test-react-vite/src/components/App/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import reactLogo from '../../assets/react.svg'
-import viteLogo from '/vite.svg'
+import viteLogo from '/vite.svg' // Viene de public
 import '../../components/App/App.css'
 
 function App() {
