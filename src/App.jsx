b1b2672852558a84be5d228869e37d50a56e7ca8
Mauricio Cox <coxmau77@gmail.com>
2024-08-16 20:22:13 -0300

    chore: Creacion de la estructura del componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
 export function App() {
   return (
-    <div>My twitter card</div>
+    <article>
+        <header>
+            <img src="https://unavatar.io/maucox" alt="Imagen de perfil de usuario" />
+            <div>
+                <strong>Mauricio Cox</strong>
+                <span>@BIT_MAU</span>
+            </div>
+        </header>
+        <aside>
+            <button>Seguir</button>
+        </aside>
+        
+    </article>
   )
 }
