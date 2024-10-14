dda0f815b13ac71bde828bf1a68113e24e21794f
Mauricio Cox <coxmau77@gmail.com>
2024-10-14 06:59:09 -0300

    Initial commit React + Vite + Tailwind CSS

--- src/Components/CreateTodo.jsx
+++
@@ -1,5 +0,0 @@
-export default function CreateTodo() {
-  return (
-    <button>CreateTodo</button>
-  )
-}
--- src/Components/TodoCounter.jsx
+++
@@ -1,9 +0,0 @@
-function TodoCounter() {
-    return (
-      <h1 className="text-3xl font-bold underline">
-        Has completado 3 de 5 TODOS
-      </h1>
-    );
-  }
-  
-  export { TodoCounter };
